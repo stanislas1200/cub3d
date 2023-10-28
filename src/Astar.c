783402043f0d9be331aa36289e5aa1e7f4dafc6c
Stanislas <sgodin@student.42.fr>
2023-10-28 18:55:04 +0200

    small clean in ASTAR

--- src/Astar.c
+++ src/Astar.c
@@ -6,17 +6,16 @@
 /*   By: sgodin <sgodin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 15:14:10 by sgodin            #+#    #+#             */
-/*   Updated: 2023/10/27 17:56:46 by sgodin           ###   ########.fr       */
+/*   Updated: 2023/10/28 18:54:42 by sgodin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
-t_anode *new_node(t_astar *a, int x, int y) {
-	t_anode *node;
-	// if (a->nodes[x][y] != NULL) { // NEVER HAPPENS ?
-	// 	printf("node already exist\n");
-	// 	return a->nodes[x][y];
-	// }
+
+t_anode	*new_node(t_astar *a, int x, int y)
+{
+	t_anode	*node;
+
 	node = malloc(sizeof(t_anode));
 	if (!node)
 		return (printf(RED "Node faill\n" RESET), NULL);
@@ -28,64 +27,99 @@ t_anode *new_node(t_astar *a, int x, int y) {
 	return (node);
 }
 
-void add_arr(t_anode **arr, t_anode *node, int i) { // protect size ?
+void	add_arr(t_anode **arr, t_anode *node, int i)
+{
 	arr[i] = node;
 }
 
-int isLegal(t_data *data, int x, int y) {
+int	is_legal(t_data *data, int x, int y)
+{
 	return (x >= 0 && y >= 0 && x < data->biggest_w && y < data->height);
 }
 
-float Heuristic(t_anode a, t_anode b) {
-	return abs(a.x - b.x) + abs(a.y - b.y);
+float	heuristic(t_anode a, t_anode b)
+{
+	return (abs(a.x - b.x) + abs(a.y - b.y));
 }
 
-void calculate(t_anode *node, t_anode *end) {
-	node->f = node->g + Heuristic(*node, *end);
+void	calculate(t_anode *node, t_anode *end)
+{
+	node->f = node->g + heuristic(*node, *end);
 }
 
-void remove_arr(t_anode *arr[], t_anode *node, int size) {
-	for (int i = 0; i <= size; i++) {
-		if (arr[i] == node) {
+void	remove_arr(t_anode *arr[], t_anode *node, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i <= size)
+	{
+		if (arr[i] == node)
+		{
 			arr[i] = arr[size - 1];
 			size--;
-			break;
+			break ;
 		}
+		i++;
 	}
 }
 
-int get_lowest_f_node(t_astar *a) {
-	int winner = 0;
-	for (int i = 0; i < a->openCount; i++) {
-		if (a->open[i]->f < a->open[winner]->f) {
+int	get_lowest_f_node(t_astar *a)
+{
+	int	winner;
+	int	i;
+
+	winner = 0;
+	i = 0;
+	while (i < a->openCount)
+	{
+		if (a->open[i]->f < a->open[winner]->f)
 			winner = i;
-		}
+		i++;
 	}
-	return winner;
+	return (winner);
 }
 
-int is_close(t_astar *a, t_anode* node) {
-	for (int i = 0; i < a->closedCount; i++) {
+int	is_close(t_astar *a, t_anode* node)
+{
+	int	i;
+
+	i = 0;
+	while (i < a->closedCount)
+	{
 		if (a->closed[i] == node)
-			return 1;
+			return (1);
+		i++;
 	}
-	return 0;
+	return (0);
 }
 
-int is_open(t_astar *a, t_anode* node) {
-	for (int i = 0; i < a->openCount; i++) {
+int	is_open(t_astar *a, t_anode* node)
+{
+	int	i;
+
+	i = 0;
+	while (i < a->openCount)
+	{
 		if (a->open[i] == node)
-			return 1;
+			return (1);
+		i++;
 	}
-	return 0;
+	return (0);
 }
 
-int is_in(t_astar *a, int y, int x) {
-	for (int i = 0; i < a->pathCount; i++) {
+int	is_in(t_astar *a, int y, int x)
+{
+	int	i;
+
+	i = 0;
+	while (i < a->pathCount)
+	{
 		if (a->path[i]->x == x && a->path[i]->y == y)
-			return 1;
+			return (1);
+		i++;
 	}
-	return 0;
+	return (0);
 }
 
 t_anode	*copy_node(t_anode *node)
@@ -97,37 +131,32 @@ t_anode	*copy_node(t_anode *node)
 		return (printf(RED "Node faill\n" RESET), NULL);
 	new_node->x = node->x;
 	new_node->y = node->y;
-	// new_node->g = node->g; // No need for path
-	// new_node->f = node->f;
-	// new_node->previous = node->previous;
 	return (new_node);
 }
 
-void print_path(t_anode **path);
-
-void make_path(t_anode *node, t_astar *a) {
+void	make_path(t_anode *node, t_astar *a)
+{
 	a->pathCount = 0;
-	while (node->previous) {
+	while (node->previous)
+	{
 		a->path[a->pathCount++] = copy_node(node);
-		if (!a->path[a->pathCount - 1]) {
-			return (free_list(a->path, a->pathCount)); // FREE path or move to here ? free to pathCount - 1 ?
-		}
+		if (!a->path[a->pathCount - 1])
+			return (free_list(a->path, a->pathCount));
 		node = node->previous;
 	}
 	a->path[a->pathCount++] = copy_node(node);
 	if (!a->path[a->pathCount - 1])
-		return (free_list(a->path, a->pathCount)); // FREE path or move to here ?
+		return (free_list(a->path, a->pathCount));
 }
 
-void find_path(t_data *data, t_astar *a) {
-	while (a->openCount > 0 && a->openCount < 5000) //limited
+void	find_path(t_data *data, t_astar *a)
+{
+	while (a->openCount > 0 && a->openCount < 5000)
 	{
 		int winner = get_lowest_f_node(a);
 		a->current = a->open[winner];
-		if (a->current->x == a->end->x && a->current->y == a->end->y) { // END
-			make_path(a->current, a);
-			return;
-		}
+		if (a->current->x == a->end->x && a->current->y == a->end->y)
+			return (make_path(a->current, a));
 		remove_arr(a->open, a->current, a->openCount--);
 		add_arr(a->closed, a->current, a->closedCount++);
 
@@ -137,7 +166,7 @@ void find_path(t_data *data, t_astar *a) {
 				if (abs(i) + abs(j) == 2) continue; // Skip diagonals
 				int new_x = a->current->x + i;
 				int new_y = a->current->y + j;
-				if (isLegal(data, new_x, new_y) && a->nodes[new_y][new_x] == NULL)
+				if (is_legal(data, new_x, new_y) && a->nodes[new_y][new_x] == NULL)
 				{
 					t_anode* neighbor = new_node(a, new_x, new_y);
 
@@ -170,7 +199,8 @@ void find_path(t_data *data, t_astar *a) {
 }
 
 
-void free_nodes(t_data *data, t_astar *a) {
+void	free_nodes(t_data *data, t_astar *a)
+{
 	if (!a->nodes)
 		return ;
 	for (int i = 0; i < data->a->size; i++) {
@@ -189,50 +219,13 @@ void free_nodes(t_data *data, t_astar *a) {
 	}
 }
 
-void free_list(t_anode *arr[], int size) {
+void	free_list(t_anode *arr[], int size)
+{
 	for (int i = 0; i < size; i++) {
 		if (arr[i] != NULL) {
 			free(arr[i]);
 			arr[i] = NULL;
 		}
-	}
-}
-
-void print_astar_map(t_data * data) {
-	printf("\x1b[H\x1b[2J");
-	// for (int i = 0; i < data->height; i++) {
-	// 	for (int j = 0; j < ft_strlen(data->map[i]); j++) {
-	// 		if (data->map[i][j] == 'X')
-	// 			printf(RED);
-	// 		else
-	// 		{
-	// 			if (i == (int)data->player.py >> 6 && j == (int)data->player.px >> 6)
-	// 				printf(GREEN);
-	// 			else
-	// 				printf(BOLD);
-	// 		}
-	// 		printf("0" RESET);
-	// 	}
-	// 	printf(" %d\n", i);
-	// }
-	for (int i = 0; i < data->a->size; i++) {
-		for (int j = 0; j < data->a->size; j++) {
-			if (data->a->nodes[i][j])
-				printf(RED);
-			else
-			{
-				if (i > 0 && i < data->height && j > 0 && j < ft_strlen(data->map[i]) && i == (int)data->player.py >> 6 && j == (int)data->player.px >> 6)
-					printf(GREEN);
-				else if (data->a->path && is_in(data->a, i, j))
-				{
-						printf(MAGENTA);
-				}
-				else
-					printf(RESET BOLD);
-			}
-			printf("0" RESET);
-		}
-		printf(" %d\n", i);
 	}
 }
 
@@ -299,39 +292,4 @@ void Astar(t_data *data, t_astar *a, int start_x, int start_y, int end_x, int en
 
 	free_list(a->open, a->openCount);
 	free_list(a->closed, a->closedCount);
-	// if (a->path && a->pathCount > 0)
-	// 	print_astar_map(data);
 }
-
-
-// int main()
-// {
-// 	t_data data;
-// 	data.height = 5;
-// 	data.biggest_w = 5;
-// 	t_astar a;
-// 	setup_astar(&data, &a);
-// 	init_astar(&data, &a);
-
-// 	a.nodes[0][1] = new_node(&a, 0, 1); // Example obstacle at row 1, col 2
-// 	// a.nodes[1][0] = new_node(&a, 1, 0); // Example obstacle at row 1, col 2
-// 	a.nodes[2][2] = new_node(&a, 2, 2); // Example obstacle at row 2, col 2  
-// 	a.nodes[2][0] = new_node(&a, 2, 0); // Example obstacle at row 2, col 2  
-// 	a.nodes[4][1] = new_node(&a, 4, 1); // Example obstacle at row 2, col 2  
-
-// 	Astar(&data, &a, 1, 1, 4, 4);
-// 	// free
-// 	free_nodes(&data, &a);
-// 	free_list(a.open, a.openCount);
-// 	free_list(a.closed, a.closedCount);
-// 	free(a.open);
-// 	free(a.closed);
-// 	print_path(a.path);
-// 	free_list(a.path, a.pathCount);
-// 	free(a.path);
-// 	for (int i = 0; i < data.height; i++) {
-// 		free(a.nodes[i]);
-// 	}
-// 	free(a.nodes);
-// 	return 0;
-// }
