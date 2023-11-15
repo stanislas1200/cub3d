a7dd774c0f4d2e948b4cec78ea92716899320807
Stanislas <sgodin@student.42.fr>
2023-11-15 14:10:14 +0100

    fix color range

--- src/bonus/is_bonus.c
+++ src/bonus/is_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sgodin <sgodin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 15:50:55 by sgodin            #+#    #+#             */
-/*   Updated: 2023/11/10 16:39:35 by sgodin           ###   ########.fr       */
+/*   Updated: 2023/11/15 14:09:51 by sgodin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,16 @@ int	is_el(char *str, t_data *d)
 
 void	check_element(t_data *d)
 {
+	int	i;
+
 	if (!d->no || !d->so || !d->we || !d->ea)
 		return (e(d, E_M RESET ": ", "Missing Texture path\n"));
 	if (d->floor[0] == -1 || d->floor[1] == -1 || d->floor[2] == -1)
 		return (e(d, E_M RESET ": ", "Missing floor color\n"));
 	if (d->ceiling[0] == -1 || d->ceiling[1] == -1 || d->ceiling[2] == -1)
 		return (e(d, E_M RESET ": ", "Missing ceiling color\n"));
+	i = -1;
+	while (++i < 3)
+		if (d->floor[i] > 255 || d->ceiling[i] > 255)
+			return (e(d, E_M RESET ": ", "Colors must be in range [0,255]\n"));
 }
